b10d4e5214a1670464734561f3e5ab6e978a0446
Francesco Lomio <francesco.lomio@gmail.com>
2022-01-14 15:43:58 +0100

    create api to access csv with results

---
+++ APIs/get_csv_api.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+# Specify directory to download from . . .
+DOWNLOAD_DIRECTORY = "/home/cloweews/Projects/CloudSea-Office-Wellbeing-Platform/results"
+
+@app.route('/getresults',methods = ['GET'])
+def get_files():
+
+    """Download a file."""
+    try:
+        return send_from_directory(DOWNLOAD_DIRECTORY, 'test_results.csv', as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 5001, threaded = True, debug = True)
