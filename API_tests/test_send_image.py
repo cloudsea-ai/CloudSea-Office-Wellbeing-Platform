37481bbf7a33a432d58a7a27c0ce5f69eda47686
Francesco Lomio <francesco.lomio@gmail.com>
2022-01-17 17:16:26 +0100

    test upload image

---
+++ API_tests/test_send_image.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import requests
+import time
+import json
+import cv2
+
+addr = 'http://localhost:5000'
+url = addr + '/api_test_image'
+
+# prepare headers for http request
+content_type = 'image/jpeg'
+# headers = {'content-type': content_type}
+
+def post_image(img_file):
+    """ post image and return the response """
+    # file_name = open('file_name.txt', 'wb')
+    # file_name.write('fra_recline.jpeg')
+    # file_name.close()
+    # img = open(img_file, 'rb').read()
+    # response = requests.post(test_url, data=img, headers=headers)
+    requests.post(url, files={'image': open(img_file, 'rb')})
+
+    # return response
+
+post_image('./sample_images/fra_recline.jpeg')
+# print(post_image('./sample_images/fra_recline.jpeg'))
+# print(response.text)
+# response = post_image('./sample_images/fra_recline.jpeg')
+# print(json.loads(response.text))
+# img = cv2.imread('lena.jpg')
+# # encode image as jpeg
+# _, img_encoded = cv2.imencode('.jpg', img)
+# # send http request with image and receive response
+# response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+# # decode response
